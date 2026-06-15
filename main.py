3eaa6b332020f9c8b0a5f986a480995435c354ef
rxuhi <hyorim4640@gmail.com>
2026-06-15 20:46:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -23,11 +23,10 @@ async def on_ready():
     await bot.load_extension("cogs.gacha")
     await bot.load_extension("cogs.role_ticket")
     
-    # 슬래시 명령어 동기화
-synced = await bot.tree.sync()
-print(f"동기화된 명령어 수: {len(synced)}")
-for cmd in synced:
-    print(cmd.name)
+    synced = await bot.tree.sync()
+    print(f"동기화된 명령어 수: {len(synced)}")
+    for cmd in synced:
+        print(cmd.name)
 
 if __name__ == "__main__":
     bot.run(BOT_TOKEN)
